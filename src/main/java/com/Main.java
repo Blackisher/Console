93ce91babd019f2e9fc13a272a51c5f06a0808a6
Blackisher <36049543+Blackisher@users.noreply.github.com>
2020-03-21 21:25:13 +0100

    Create Main.java

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,57 @@
+package com;
+
+import org.jline.keymap.KeyMap;
+import org.jline.reader.*;
+import org.jline.terminal.Size;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args){
+        Terminal terminal = null;
+        try {
+            terminal = TerminalBuilder.builder()
+                    .system(false)
+                    .streams(System.in, System.out)
+                    .size(new Size(80, 80))
+                    .build();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LineReader reader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+        MyWidgets myWidgets = new MyWidgets(reader);
+        KeyMap<Binding> keyMap = reader.getKeyMaps().get("main");
+        keyMap.bind(new Reference("_plus7"), "k");
+
+        while (true) {
+            reader.readLine("", "", (MaskingCallback) null, null);
+        }
+    }
+
+}
+
+class MyWidgets extends org.jline.builtins.Widgets {
+    private LineReader reader;
+
+    public MyWidgets(LineReader reader) {
+        super(reader);
+        this.reader = reader;
+        addWidget("_plus7", this::plus7);
+    }
+
+    public boolean plus7() {
+        try {
+            Integer num = Integer.parseInt(buffer().toString()) + 7;
+            buffer().clear();
+            buffer().write(num.toString());
+            callWidget(LineReader.REDRAW_LINE);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
